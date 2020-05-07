d77ca83363f353441c943ae1b3816277ca2ef2b4
peterkuria <peter.kuria.cloud@gmail.com>
2020-05-08 00:01:59 +0300

    bootstrapped django backbone in /tutor

---
+++ tutor/tutor/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from tutor.users.models import User
+from tutor.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
