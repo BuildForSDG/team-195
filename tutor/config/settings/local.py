984369ee89908e95c6aa86b0ff1e66d67ef0ba1a
Andrew <njayaandrew@gmail.com>
2020-05-23 22:22:06 +0300

    [chore] Added database enviroment variables.

--- tutor/config/settings/local.py
+++ tutor/config/settings/local.py
@@ -11,7 +11,9 @@ SECRET_KEY = env(
     default="cOZbXTiznCxUnKYzic4MH4bUoUQ1jFO6UAI8KaDljgtnz1NPewjTjMKBSVC7Gb38",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = [
+    "localhost", "0.0.0.0", "127.0.0.1", '[::1]', ".herokuapp.com"
+]
 
 # CACHES
 # ------------------------------------------------------------------------------
